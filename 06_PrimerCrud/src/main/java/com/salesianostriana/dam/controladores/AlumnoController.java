da2155564c4667e5f4f0c513a756c95de3f602b6
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2022-05-11 14:26:48 +0200

    Primer crud

--- 06_PrimerCrud/src/main/java/com/salesianostriana/dam/controladores/AlumnoController.java
+++ 06_PrimerCrud/src/main/java/com/salesianostriana/dam/controladores/AlumnoController.java
@@ -1,13 +1,21 @@
 package com.salesianostriana.dam.controladores;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.salesianostriana.dam.modelo.Alumno;
 import com.salesianostriana.dam.servicio.AlumnoServicio;
 
 @Controller
+@RequestMapping("/alumno")
 public class AlumnoController {
 	
 	@Autowired
@@ -18,6 +26,33 @@ public class AlumnoController {
 		model.addAttribute("lista", alumnoServicio.findAll());
 		return "alumno/index";
 	}
+	
+	@GetMapping("/detalle/{id}")
+	public String detail(Model model, @PathVariable Long id) {
+		
+		Optional<Alumno> result = alumnoServicio.findById(id);
+		
+		if (result.isPresent()) {
+			model.addAttribute("alumno", result.get());
+			return "alumno/detail";			
+		} else {
+			return "redirect:/alumno/";
+		}
+		
+	}
+	
+	@GetMapping("/nuevo")
+	public String showForm(Model model) {
+		model.addAttribute("alumno", new Alumno());
+		return "alumno/nuevo";
+	}
+	
+	@PostMapping("/nuevo/submit")
+	public String saveAlumno(@ModelAttribute("alumno") Alumno nuevo) {
+		alumnoServicio.save(nuevo);
+		return "redirect:/alumno/";
+	}
+	
 	
 	
 
