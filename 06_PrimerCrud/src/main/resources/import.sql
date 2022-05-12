03bd782335464bdde14f8663ba2c5483aeaeb301
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2022-05-12 09:30:29 +0200

    Ejemplo de import.sql·

---
+++ 06_PrimerCrud/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO ALUMNO (id, nombre, apellidos) VALUES (NEXTVAL('hibernate_sequence'),'Pepe','Pérez'); 
+INSERT INTO ALUMNO (id, nombre, apellidos) VALUES (NEXTVAL('hibernate_sequence'),'Juan','Gómez'); 
