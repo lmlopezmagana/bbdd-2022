c34a8db2f47d8d0fe37ad4f2b2ed1d4128143f90
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2022-05-17 10:06:29 +0200

    Import.sql

--- 07_Consultas/src/main/resources/import.sql
+++ 07_Consultas/src/main/resources/import.sql
@@ -1,2 +1,9 @@
-INSERT INTO ALUMNO (id, nombre, apellidos) VALUES (NEXTVAL('hibernate_sequence'),'Pepe','Pérez'); 
-INSERT INTO ALUMNO (id, nombre, apellidos) VALUES (NEXTVAL('hibernate_sequence'),'Juan','Gómez'); 
+-- INSERT INTO ALUMNO (id, nombre, apellidos) VALUES (NEXTVAL('hibernate_sequence'),'Pepe','Pérez'); 
+-- INSERT INTO ALUMNO (id, nombre, apellidos) VALUES (NEXTVAL('hibernate_sequence'),'Juan','Gómez'); 
+
+
+INSERT INTO CURSO (id, nombre) VALUES (1, '1º DAM');
+
+INSERT INTO ALUMNO (id, nombre, apellidos, curso_id) VALUES (1, 'Pepe', 'Pérez', 1);
+
+ALTER SEQUENCE hibernate_sequence RESTART WITH 1000;
